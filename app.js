5416e9ad2ceeb42d2db473560b3ac063cf0ab94e
git status <yzou42@wisc.edu>
2019-07-18 17:54:21 -0400

    sets up the project

---
+++ app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<p>React lives!</p>, document.getElementById("root"));
+
+// Anything that we can write in code the browser doesn't understand can also
+// be written exclusively in code that it does.
+// React.createElement('p', null, 'Hi')
