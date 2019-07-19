11fb59ffb8e6e3303286db55c6157e7e7363fe73
git status <yzou42@wisc.edu>
2019-07-19 15:03:24 -0400

    build a todo list displaying dummy data

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,4 +1,61 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
+/* eslint-disable no-tabs */
+/* eslint-disable react/jsx-filename-extension */
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>React lives!</p>, document.getElementById("root"));
+const dummyData = ['to eat', 'to sleep', 'to code'];
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    // const [data] = props.data;
+  }
+
+  render() {
+    return (
+      <li>
+        <button className="delete-btn">x</button>
+        {this.props.data}
+      </li>
+    );
+  }
+}
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <ul>
+        {dummyData.map(data => (
+          <Todo key={data} data={data} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+// class InputLine extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {  }
+//     }
+//     render() {
+//         return (  );
+//     }
+// }
+
+// class TodoApp extends Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {};
+// 	}
+// 	render() {
+// 		return <h1>Hello</h1>;
+// 	}
+// }
+
+ReactDOM.render(<TodoList />, document.getElementById('root'));
