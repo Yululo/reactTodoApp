294b066291c6cb94d0a5ee8051c9eb865775e7b8
git status <yzou42@wisc.edu>
2019-07-20 17:28:02 -0400

    adds connection to mongoose

--- backend/routes/databaseAccess.js
+++ backend/routes/databaseAccess.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const TodoItem = require('../models/TodoItem');
 
+// router.get('/add', (req, res) => {
+//   res.send('Hello');
+// });
 router.get('/add', (req, res) => {
-  res.send('Hello');
+  const testTodo = new TodoItem({
+    task: 'test task',
+    completed: 'false'
+  });
+
+  testTodo
+    .save()
+    .then(response => {
+      res.send(response);
+    })
+    .catch(error => {
+      res.send(error);
+    });
 });
 
 module.exports = router;
