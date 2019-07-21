31015e92fc5556d1f96f2c71934d39af6f2b392e
git status <yzou42@wisc.edu>
2019-07-21 17:15:52 -0400

    complete all functionality for this todo App

--- backend/routes/databaseAccess.js
+++ backend/routes/databaseAccess.js
@@ -20,4 +20,35 @@ router.post('/add', (req, res) => {
     });
 });
 
+router.get('/all', (req, res) => {
+  TodoItem.find({}, (err, items) => {
+    if (err) res.send(err);
+    if (!items) res.send('Items not found');
+
+    res.send(items);
+  });
+});
+
+router.post('/remove', (req, res) => {
+  TodoItem.findByIdAndRemove(req.body._id, (err, item) => {
+    if (err) res.send(err);
+    if (!item) res.send('Todo item attempting to remove is not found');
+
+    res.send('Success!');
+  });
+});
+
+router.post('/toggle', (req, res) => {
+  TodoItem.findById(req.body._id, (err, item) => {
+    if (err) res.send(err);
+    if (!item) res.send('Todo item attempting to remove is not found');
+
+    // to toggle the item
+    item.completed = !item.completed;
+    item.save((err, item) => {
+      res.send(item);
+    });
+  });
+});
+
 module.exports = router;
